b445fffcf93f1cd1c32bbfb908c0b55aa22372e5
Emmanuel Guefif <eguefif@fastmail.com>
2025-03-13 21:02:58 -0400

    CHATEX-3: add agent monitoring

--- apps/chat_clients/lib/chat_clients.ex
+++ apps/chat_clients/lib/chat_clients.ex
@@ -25,27 +25,31 @@ defmodule ChatClients do
   # Server API
   @impl true
   def init(:ok) do
-    {:ok, %{}}
+    clients = %{}
+    refs = %{}
+    {:ok, {clients, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, id}, _from, clients) do
+  def handle_call({:lookup, id}, _from, {clients, _}) do
     {:reply, Map.get(clients, id), clients}
   end
 
   @impl true
-  def handle_call({:add_client, id}, _from, clients) do
+  def handle_call({:add_client, id}, _from, {clients, refs}) do
     if Map.has_key?(clients, id) do
       {:noreply, id}
     else
       {:ok, client} = Chat.ChatClient.start_link(:ok)
       clients = Map.put(clients, id, client)
-      {:reply, id, clients}
+      ref = Process.monitor(client)
+      refs = Map.put(refs, ref, id)
+      {:reply, id, {clients, refs}}
     end
   end
 
   @impl true
-  def handle_call({:get_message, id}, _from, clients) do
+  def handle_call({:get_message, id}, _from, {clients, _}) do
     if Map.has_key?(clients, id) do
       client = Map.get(clients, id)
       messages = Chat.ChatClient.get_messages(client)
@@ -56,7 +60,7 @@ defmodule ChatClients do
   end
 
   @impl true
-  def handle_call({:add_message, id, message}, _from, clients) do
+  def handle_call({:add_message, id, message}, _from, {clients, _}) do
     if Map.has_key?(clients, id) do
       client = Map.get(clients, id)
       Chat.ChatClient.add_messages(client, message)
@@ -64,5 +68,19 @@ defmodule ChatClients do
     else
       {:reply, {:error, :key_error, id}, clients}
     end
+  end
+
+  @impl true
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {clients, refs}) do
+    {id, refs} = Map.pop(refs, ref)
+    names = Map.pop(clients, id)
+    {:noreply, {names, refs}}
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    require Logger
+    Logger.debug("Unexpected message in ChatClients: #{inspect(msg)}")
+    {:noreply, state}
   end
 end
