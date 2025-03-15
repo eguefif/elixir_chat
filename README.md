08ba9a7ec19d5b06d96e2f53b93a3db601ce6016
Emmanuel Guefif <97660236+eguefif@users.noreply.github.com>
2025-03-15 16:29:43 -0400

    CHATEX-5: implements solution (#10)

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # Chat
 
-**TODO: Add description**
+## Description
 
+Learning project. The knowledge goals are:
+* Agent
+* GenServer
+* Tree Supervisor
+
+## Getting Started
+
+### Dependencies
+
+* Elixir
+
+
+### Executing program
+
+* Server:
+```
+run mix --no-halt
+```
+
+* Client
+There is no client. You need to use telenet 127.0.0.1 4040. Here a list of supported commands:
+* NAME your_name
+* WHISPER interlocutor_name message
--- apps/chat_clients/lib/chat_clients.ex
+++ apps/chat_clients/lib/chat_clients.ex
@@ -28,63 +28,57 @@ defmodule ChatClients do
   @impl true
   def init(:ok) do
     clients = %{}
+    clients_name = %{}
     refs = %{}
-    {:ok, {clients, refs}}
+    {:ok, {clients, refs, clients_name}}
   end
 
   @impl true
-  def handle_call({:add_client, name}, {pid, _}, {clients, refs}) do
+  def handle_call({:add_client, name}, {pid, _}, {clients, refs, clients_name}) do
     if Map.has_key?(clients, pid) do
       {:noreply, {clients, refs}}
     else
       {:ok, client} = ChatClient.start_link(:ok)
       ChatClient.set_name(client, name)
       clients = Map.put(clients, pid, client)
+      clients_name = Map.put(clients_name, name, client)
       ref = Process.monitor(client)
       refs = Map.put(refs, ref, pid)
       require Logger
       Logger.info("Adding client Genserver: #{inspect(clients)}")
-      {:reply, pid, {clients, refs}}
+      {:reply, pid, {clients, refs, clients_name}}
     end
   end
 
   @impl true
-  def handle_call({:get_message}, {pid, _}, {clients, refs}) do
+  def handle_call({:get_message}, {pid, _}, {clients, refs, clients_name}) do
     if Map.has_key?(clients, pid) do
       client = Map.get(clients, pid)
       messages = ChatClient.get_messages(client)
-      {:reply, messages, {clients, refs}}
+      {:reply, messages, {clients, refs, clients_name}}
     else
-      {:reply, {:error, :key_error, pid}, {clients, refs}}
+      {:reply, {:error, :key_error, pid}, {clients, refs, clients_name}}
     end
   end
 
   @impl true
-  def handle_call({:add_message, interlocutor, message}, _, {clients, refs}) do
-    with clients_list <- Map.to_list(clients),
-         {_, client} <-
-           Enum.find(clients_list, fn {_, client} ->
-             ChatClient.is_client(client, interlocutor)
-           end) do
-      Logger.debug("Found client: #{inspect(client)}")
-      ChatClient.add_message(client, message)
-      {:reply, :ok, {clients, refs}}
-    else
-      _ -> {:noreply, {clients, refs}}
-    end
+  def handle_call({:add_message, interlocutor, message}, _, {clients, refs, clients_name}) do
+    client = Map.get(clients_name, interlocutor)
+    ChatClient.add_message(client, message)
+    {:reply, :ok, {clients, refs, clients_name}}
   end
 
   @impl true
-  def handle_call({:get_name}, {pid, _}, {clients, refs}) do
+  def handle_call({:get_name}, {pid, _}, {clients, refs, clients_name}) do
     client = Map.get(clients, pid)
-    {:reply, ChatClient.get_name(client), {clients, refs}}
+    {:reply, ChatClient.get_name(client), {clients, refs, clients_name}}
   end
 
   @impl true
-  def handle_info({:DOWN, ref, :process, _pid, _reason}, {clients, refs}) do
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {clients, refs, clients_name}) do
     {id, refs} = Map.pop(refs, ref)
     names = Map.pop(clients, id)
-    {:noreply, {names, refs}}
+    {:noreply, {names, refs, clients_name}}
   end
 
   @impl true
